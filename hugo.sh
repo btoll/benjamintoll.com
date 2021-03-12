41a2cf244830e812e89af6c39fa4455cdaeee815
Benjamin Toll <benjam72@yahoo.com>
2021-03-12 00:25:47 -0500

    Simplified hugo.sh by removing SERVER option

--- hugo.sh
+++ hugo.sh
@@ -4,22 +4,13 @@ set -e
 
 BASE_URL="${BASE_URL:=/}"
 DESTINATION="${DESTINATION:=/out}"
-SERVE="${SERVE:=false}"
 THEME="${THEME:=lithium}"
 
 echo
 echo "BASE_URL: $BASE_URL"
 echo "DESTINATION: $DESTINATION"
-echo "SERVE: $SERVE"
 echo "THEME: $THEME"
 echo
 
-HUGO=/usr/local/bin/hugo
-
-if ! $SERVE
-then
-    $HUGO --source "/src" --theme "$THEME" --destination "$DESTINATION" --baseURL "$BASE_URL"
-else
-    $HUGO server --source "/src" --theme "$THEME" --destination "$DESTINATION" --baseURL "$BASE_URL" --bind "0.0.0.0"
-fi
+/usr/local/bin/hugo --source "/src" --theme "$THEME" --destination "$DESTINATION" --baseURL "$BASE_URL"
 
