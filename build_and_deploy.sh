89bc5caba4ae2acb40e1ce11a5ebc44859475457
Benjamin Toll <benjam72@yahoo.com>
2021-03-28 19:34:30 -0400

    Added Dockerfile and build and deploy script

---
+++ build_and_deploy.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+GREEN_FG=$(tput setaf 2 2>/dev/null)
+RED_FG=$(tput setaf 1 2>/dev/null)
+END_FG_COLOR=$(tput sgr0 2>/dev/null)
+
+DEPLOY=false
+DOCKER_ID=btoll
+FULL_IMAGE_NAME=
+IMAGE_NAME=benjamintoll.com
+TAG_NAME=()
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        -n | --name)
+            shift
+            IMAGE_NAME="$1"
+            ;;
+        -d | --deploy)
+            DEPLOY=true
+            ;;
+        -t | --tagname)
+            shift
+            TAG_NAME+=("$1")
+            ;;
+        *)
+            echo "Unknown option" "$1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+build_image () {
+    if ! docker run --rm -it -v "$(pwd)":/src btoll/hugo:0.80.0
+    then
+        echo -e "\n$RED_FG[$0]$END_FG_COLOR The $FULL_IMAGE_NAME site could not be generated."
+        exit 1
+    fi
+
+    if ! docker build -t "$FULL_IMAGE_NAME" .
+    then
+        echo -e "\n$RED_FG[$0]$END_FG_COLOR The image $FULL_IMAGE_NAME could not be built."
+        exit 1
+    fi
+
+    echo -e "\n$GREEN_FG[$0]$END_FG_COLOR Successfully built $FULL_IMAGE_NAME."
+}
+
+for item in ${TAG_NAME[*]}
+do
+    FULL_IMAGE_NAME="$DOCKER_ID/$IMAGE_NAME:$item"
+
+    build_image
+
+    if $DEPLOY
+    then
+        if ! docker push "$FULL_IMAGE_NAME"
+        then
+            echo -e "\n$RED_FG[$0]$END_FG_COLOR \`docker push\` failed for $FULL_IMAGE_NAME"
+            exit 1
+        fi
+        echo -e "\n$GREEN_FG[$0]$END_FG_COLOR Successfully pushed $FULL_IMAGE_NAME to Docker Hub."
+    fi
+done
+
+echo "$GREEN_FG[$0]$END_FG_COLOR Operations completed with no failures."
+
